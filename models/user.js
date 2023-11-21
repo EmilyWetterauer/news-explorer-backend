cf4ac2f89f0d26731bc229f3b82dc2a65f61a7f2
Emma Wetterauer <emmawetterauer@gmail.com>
2023-11-20 19:08:03 -0700

    added API resource schemas and models

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+    default: "Elise Bouer",
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "You must enter a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
